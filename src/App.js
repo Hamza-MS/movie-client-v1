0c3ea027c21cabfb8466a6162854ab7ca4b9773f
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-04 12:21:35 +0100

    testing axios by calling getMovies endpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import api from './api/axiosConfig';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      setMovies(response.data);
+      console.log(response.data);
+    }catch (error) {
+      console.log(error);
+    }
+    
+  }
+
+ useEffect(() => { getMovies() }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+hello world
     </div>
   );
 }
