ee4dcde57f3d5471cea8f08a931b7efbbd4dcb24
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-04 23:09:03 +0100

    add trailer component
    the trailer component  launch react video player

---
+++ src/components/trailer/trailer.js
@@ -0,0 +1,17 @@
+import React from "react";  
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+
+const Trailer = (   ) => {
+    let params = useParams();
+    const key = params.ytTrailerId;
+
+    return (
+        <div className="react-player-wrapper">
+            { (key!=null)?
+                <ReactPlayer controls={true} url={`https://www.youtube.com/watch?v=${key}`} width='100%' height='100%'/>:null
+            }
+        </div>
+
+    );
+}
