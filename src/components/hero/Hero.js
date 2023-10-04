a6bafe2d8deae07fe890a9857a4a01f9b320ebf2
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-04 23:07:55 +0100

    Update Hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
-import { Paper } from '@material-ui/core';
+import { Paper } from '@mui/material';
 
 export const Hero = ({movies}) => {
   return (
@@ -11,7 +11,8 @@ export const Hero = ({movies}) => {
                 movies.map((movie)=>{
                     return(
                         <Paper key={movie.id}>
-                            <img src={movie.poster} alt={movie.title} className="hero-image"/>
+                            <img src={movie.poster} alt="" className="hero-image"/>
+                            <h4>{movie.title}</h4>
                         </Paper>
                     )
                 })
