8d93401a51ab7e7fbfb82c16b185964c15790430
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-09 20:05:09 +0100

    add trailer route with a parameter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
 import { Home } from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+
 function App() {
 
   const [movies, setMovies] = useState([]);
@@ -27,6 +29,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Layout />} >
+          <Route path="/" element={<Home movies={movies}/>} />
+          <Route path='/trailer/:ytTrailerId' element={<Trailer />}/>
         </Route>
       </Routes>
 
