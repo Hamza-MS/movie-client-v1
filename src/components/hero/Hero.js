b5aed2846d3501ca6be23cec11f531a14f41b3b3
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-09 23:53:11 +0100

    styling the hero section

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,22 +2,33 @@ import React from 'react';
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
-export const Hero = ({movies}) => {
-  return (
-    <div>
-        <Carousel>
-            {
-                movies.map((movie)=>{
-                    return(
-                        <Paper key={movie.id}>
-                            <img src={movie.poster} alt="" className="hero-image"/>
-                            <h4>{movie.title}</h4>
-                        </Paper>
-                    )
-                })
-            }
-        </Carousel>
-    </div>
-  )
-}
+export const Hero = ({ movies }) => {
+    return (
+            <Carousel>
+                {movies.map((movie) => {
+                    return (
+                        <div className='hero-container'>
+                            <Paper key={movie.id} className="hero-carousel-paper">
+                                <div className="hero-background-image" style={{ backgroundImage: `url(${movie.backdrops[1]})` }}>
+                                    <div className='detail-container' >
+                                        <div className='movie-poster-container' style={{backgroundImage:`url(${movie.poster})`}}/>
+                                        <h4 className="hero-movie-title">{movie.title}</h4>
+                                        <div className="play-button-container">
+                                            <Link to={`/trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                <FontAwesomeIcon icon={faCirclePlay} className="play-button-icon" />
+                                            </Link>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </Paper>
+                        </div>
+                    );
+                })}
+            </Carousel>
+    );
+};
