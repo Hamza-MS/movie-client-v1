99a4c7e9b7be1f3d0c209c0d59f8938fca43b9ae
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-21 15:22:01 +0100

    fix the header and add opacity

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" className='golden-header'>
-            <Navbar.Brand href="#home">Movie Client</Navbar.Brand>
+        <div className='navBarContainer'>
+        <Navbar className="golden-header" bg="dark" variant="dark" expand="lg" >
+            <Link className="link" to="/"> <Navbar.Brand >Movie Client</Navbar.Brand></Link>
+           
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#movies">Movies</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link  >  <Link className="link" to="/">Home</Link></Nav.Link> 
+             <Nav.Link ><Link className="link" to="/">Movies</Link></Nav.Link>
+             <Nav.Link ><Link className="link" to="/">About</Link></Nav.Link>
+
+                    
+                    
+                    
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
+        </div>
     );
 }
 
