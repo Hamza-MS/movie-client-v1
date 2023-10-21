141517b14ec7d5d17b33a6e16ff9b26bcb98c413
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-21 15:22:29 +0100

    give the trailler a full screen size

--- src/components/trailer/trailer.js
+++ src/components/trailer/trailer.js
@@ -1,6 +1,7 @@
 import React from "react";  
 import { useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
+import  './Trailer.css';
 
 const Trailer = (   ) => {
     let params = useParams();
