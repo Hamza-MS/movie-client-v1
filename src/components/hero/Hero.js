95d416162af5d21ab9bd231eada8f3cb19af9d03
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-21 15:22:46 +0100

    add a review button

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,6 +5,7 @@ import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 export const Hero = ({ movies }) => {
     return (
@@ -20,6 +21,11 @@ export const Hero = ({ movies }) => {
                                         <div className="play-button-container">
                                             <Link to={`/trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
                                                 <FontAwesomeIcon icon={faCirclePlay} className="play-button-icon" />
+                                            </Link>
+                                        </div>
+                                        <div className='review-btn-container'>
+                                            <Link to ={`/review/${movie.imdbId}`}>
+                                                <Button variant='warning' className='review-btn'>review</Button>
                                             </Link>
                                         </div>
                                         
